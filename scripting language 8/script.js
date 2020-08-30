151bae760d181a42959d7ed5288879a1de938146
karn767 <57304172+karn767@users.noreply.github.com>
2020-08-30 11:25:01 -0700

    Add files via upload

---
+++ scripting language 8/script.js
@@ -0,0 +1,7 @@
+var laptopCompanies = new Array('Samsung', 'Apple', 'Google', 'Lenovo', 'LG');
+console.log("Length of laptopCompanies array:", laptopCompanies.length);
+laptopCompanies.push('Dell');
+laptopCompanies.push('Sony');
+console.log('laptopCompanies array:', laptopCompanies);
+laptopCompanies.pop();
+console.log("laptopCompanies :", laptopCompanies);
---
+++ scripting language 9/script.js
@@ -0,0 +1,8 @@
+var mobileCompanies = {
+    'apple': 2, 
+    'samsung': 5,
+    'motorolla': 2,
+    'google': 3
+}
+delete mobileCompanies['apple'];
+console.log("mobileCompanies object :", mobileCompanies);
